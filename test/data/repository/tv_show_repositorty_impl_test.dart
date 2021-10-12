1983cf329e332d0eefe0fc670137b87f5fb5513f
Daniel Villamizar <Spiro94@users.noreply.github.com>
2021-10-12 09:08:53 -0500

    test: Tests añadidos para getSeasos

--- test/data/repository/tv_show_repositorty_impl_test.dart
+++ test/data/repository/tv_show_repositorty_impl_test.dart
@@ -4,6 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:mocktail/mocktail.dart';
 import 'package:netflapp/core/failures/failure.dart';
 import 'package:netflapp/core/utils/constants.dart';
+import 'package:netflapp/data/models/tv_show_episode_model.dart';
 import 'package:netflapp/data/models/tv_show_model.dart';
 import 'package:netflapp/data/repository/tv_show_repositorty_impl.dart';
 
@@ -55,11 +56,10 @@ void main() {
       //act
       final result = await tvShowRepositoryImpl.getPopular();
       var list = [];
-      if (result.isRight()) {
-        result.fold((l) => null, (r) => list = r);
-        //assert
-        expect(list, tPopulars);
-      }
+
+      result.fold((l) => null, (r) => list = r);
+      //assert
+      expect(list, tPopulars);
     });
 
     test('should return a failure when the response code is 404 or other',
@@ -74,11 +74,10 @@ void main() {
       //act
       final result = await tvShowRepositoryImpl.getPopular();
       Failure? failure;
-      if (result.isLeft()) {
-        result.fold((l) => failure = l, (r) => null);
-        //assert
-        expect(failure, Failure());
-      }
+
+      result.fold((l) => failure = l, (r) => null);
+      //assert
+      expect(failure, Failure());
     });
   });
 
@@ -114,11 +113,10 @@ void main() {
       //act
       final result = await tvShowRepositoryImpl.getRecommended();
       var list = [];
-      if (result.isRight()) {
-        result.fold((l) => null, (r) => list = r);
-        //assert
-        expect(list, tRecommended);
-      }
+
+      result.fold((l) => null, (r) => list = r);
+      //assert
+      expect(list, tRecommended);
     });
 
     test('should return a failure when the response code is 404 or other',
@@ -133,11 +131,10 @@ void main() {
       //act
       final result = await tvShowRepositoryImpl.getRecommended();
       Failure? failure;
-      if (result.isLeft()) {
-        result.fold((l) => failure = l, (r) => null);
-        //assert
-        expect(failure, Failure());
-      }
+
+      result.fold((l) => failure = l, (r) => null);
+      //assert
+      expect(failure, Failure());
     });
   });
 
@@ -173,6 +170,109 @@ void main() {
       call.fold((l) => null, (r) => favorites = r);
 
       expect(favorites, tFavorites);
+    });
+  });
+
+  group('GetSeasons', () {
+    List<TvShowEpisodeModel> tEpisodes = [
+      const TvShowEpisodeModel(
+        3062390,
+        'What If… T\'Challa Became a Star-Lord?',
+        1,
+        7.423,
+        'The rough-and-tumble space pirates known as the Ravagers abduct T’Challa instead of Peter Quill.',
+        '/urF7o2y7sG6GB53R0krSk9krw0E.jpg',
+        1,
+        '2021-08-11',
+      )
+    ];
+
+    TvShowModel tShow = TvShowModel(
+      91363,
+      '/lztz5XBMG1x6Y5ubz7CxfPFsAcW.jpg',
+      'What If...?',
+      8.5,
+      1,
+      1,
+      false,
+      tEpisodes,
+    );
+
+    void setUpMockHttpClientSuccess200() {
+      when(() => mockHttpClient.get(Uri.parse(
+              url + '/tv/${tShow.id}?api_key=$movieApiKey&language=en-US')))
+          .thenAnswer((_) async =>
+              http.Response(jsonReader('details.json'), 200, headers: {
+                HttpHeaders.contentTypeHeader:
+                    'application/json; charset=utf-8',
+              }));
+
+      when(() => mockHttpClient.get(Uri.parse(url +
+              '/tv/${tShow.id}/season/1?api_key=$movieApiKey&language=en-US')))
+          .thenAnswer((_) async =>
+              http.Response(jsonReader('seasons.json'), 200, headers: {
+                HttpHeaders.contentTypeHeader:
+                    'application/json; charset=utf-8',
+              }));
+    }
+
+    void setUpMockHttpClient400Failure() {
+      when(() => mockHttpClient.get(Uri.parse(
+              url + '/tv/${tShow.id}?api_key=$movieApiKey&language=en-US')))
+          .thenAnswer(
+              (_) async => http.Response('Error ocurred', 404, headers: {
+                    HttpHeaders.contentTypeHeader:
+                        'application/json; charset=utf-8',
+                  }));
+
+      when(() => mockHttpClient.get(Uri.parse(url +
+              '/tv/${tShow.id}/season/1?api_key=$movieApiKey&language=en-US')))
+          .thenAnswer(
+              (_) async => http.Response('Error ocurred', 404, headers: {
+                    HttpHeaders.contentTypeHeader:
+                        'application/json; charset=utf-8',
+                  }));
+    }
+
+    test('should perform a GET request on the endpoint URLs', () async {
+      //arrange
+      setUpMockHttpClientSuccess200();
+
+      //act
+      await tvShowRepositoryImpl.getSeasons(tShow.id);
+      //assert
+
+      verify(() => mockHttpClient.get(Uri.parse(
+          url + '/tv/${tShow.id}?api_key=$movieApiKey&language=en-US')));
+      verify(() => mockHttpClient.get(Uri.parse(url +
+          '/tv/${tShow.id}/season/1?api_key=$movieApiKey&language=en-US')));
+    });
+
+    test(
+        'should return a TvShowModel with the list of episodes when the response code is 200 (success)',
+        () async {
+      //arrange
+      setUpMockHttpClientSuccess200();
+      //act
+      final result = await tvShowRepositoryImpl.getSeasons(tShow.id);
+      TvShowModel? tvShowModelRes;
+
+      result.fold((l) => null, (r) => tvShowModelRes = r);
+      //assert
+      expect(tvShowModelRes, tShow);
+    });
+
+    test('should return a failure when the response code is 404 or other',
+        () async {
+      //arrange
+      setUpMockHttpClient400Failure();
+      //act
+      final result = await tvShowRepositoryImpl.getSeasons(tShow.id);
+      Failure? failure;
+
+      result.fold((l) => failure = l, (r) => null);
+      //assert
+      expect(failure, Failure());
     });
   });
 }
